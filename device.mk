196194d8ae706c359c8590abad4df2452a969faa
Vaisakh Murali <mvaisakh@statixos.com>
2022-06-09 14:14:08 +0300

    merlinx: Import SpeechVol Param for modifications
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: Ibf408624613f9b62d35a8ce56113bc75fa885380

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-xd
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/SpeechVol_AudioParam.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_param/SpeechVol_AudioParam.xml
+
 # NFC stack (AOSP)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_merlinnfc/android.hardware.nfc.xml \
